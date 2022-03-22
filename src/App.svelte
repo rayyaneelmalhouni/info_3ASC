c4b0ea83482af354630eb534b7aa834fce9e7f70
RayyaneElmalhouni <elmalhounirayyane@gmail.com>
2022-03-22 19:32:53 +0100

    liasions

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Navbar from "./components/Navbar.svelte";
+	import Home from "./components/Home.svelte";
+	import AboutUs from "./components/AboutUs.svelte";
+	import Projects from "./components/Projects.svelte";
+	import ContactUs from "./components/ContactUs.svelte";
+	import Footer from 	"./components/Footer.svelte";
+
+</script>
+<Navbar />
+<Home />
+<AboutUs />
+<Projects />
+<ContactUs />
+<Footer />
---
+++ src/components/Projects.svelte
@@ -0,0 +1,8 @@
+<script>
+   import Project from "./Project.svelte";
+   const projects_data = [1, 2, 3, 4]
+</script>
+
+{#each projects_data as project}
+    <Project />
+{/each}
